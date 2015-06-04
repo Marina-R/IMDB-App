1004a3c02e78b32b3e932821a2c8b67f341c9cbe
maryna <luv_this_world@hotmail.com>
2015-06-03 21:41:19 -0500

    IMDB App 2

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -28,10 +28,10 @@ $(document).ready(function() {
 	var myWatchList = [];
 	var toWatch = {};
 
-	function checkID (watchListArray, imdbID) {
+	function checkID (watchListArray, title) {
 		var present = false;
 		for(var j = 0; j < watchListArray.length; j++) {
-			if(watchListArray[j].imdbID == imdbID) {
+			if(watchListArray[j].Title == title) {
 				present = true;
 				break;
 			}
@@ -39,42 +39,47 @@ $(document).ready(function() {
 		return present;
 	}
 
+	var searchList = _.template($('#list-template').html());
+	var watchMovies = _.template($('#watch-template').html());
+
+
 	$('.search-form').submit(function(e) {
 		e.preventDefault();
 		var findMovie = $(e.target).find('.search-box').val();
 		thisApp.navigate('results/' + findMovie, {trigger: true});
 
 		function getMovie (movie) {
-			var movieList = '';
+			$('#list').empty();
 			for(var i = 0; i < movie.Search.length; i++) {
-				var movies = [];
-				var alreadyChecked = checkID (myWatchList, movie.Search[i].imdbID);
-				var opacity = '';
-
+				var alreadyChecked = checkID(myWatchList, movie.Search[i].Title);
+				movie.Search[i].opacity = '';
+				
 				if(alreadyChecked) {
-					opacity = ' style="opacity:0.5"';
+					movie.Search[i].opacity = ' style="opacity:0.5"';
 				}
-				for(var prop in movie.Search[i]) {
 
-					movies.push(' ' + prop + ': ' + movie.Search[i][prop]);
-				}
-				movies = movies.toString();
-				movieList += '<li data-position ="' + i + '"' + opacity + '>' + movies + '</li>';
+				movie.Search[i].pos = i;
+				var listRow = searchList(movie.Search[i]);
+				console.log(listRow);
+				$('#list').append(listRow);
 			}
-		
-			$('#list').html(movieList);
+
 			$('#list').fadeIn(1000);
+
 
 			$('li').click(function(e) {
 				$(this).css('opacity', '0.5');
 
 				var thisPosition = $(this).data('position');
-				var onList = checkID (myWatchList, movie.Search[thisPosition].imdbID);
+				var onList = checkID(myWatchList, movie.Search[thisPosition].Title);
 				
 				if(!onList) {
 					myWatchList.push(movie.Search[thisPosition]);
+					console.log(myWatchList);
+					var myWatch = watchMovies(movie.Search[thisPosition]);
+					$('#movie-time').append(myWatch);
+					$('#watch').fadeIn(1000);
 				}
-			console.log(myWatchList);	
 			});	
 		};
 
